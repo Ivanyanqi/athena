85bf9c5141eefb028594e1a177303b79b6e9df09
Ivanyanqi <yuwu889@163.com>
2019-11-02 20:13:52 +0800

    init project

---
+++ athena-api/src/main/java/cn/ivan/athenaapi/AthenaApiApplication.java
@@ -0,0 +1,13 @@
+package cn.ivan.athenaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthenaApiApplication.class, args);
+	}
+
+}
